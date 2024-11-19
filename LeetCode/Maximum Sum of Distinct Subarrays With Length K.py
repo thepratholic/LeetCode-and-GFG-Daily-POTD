c701cdf09a3f40a068b3a24915d24a2012eec12b
prathammkc <chelaramanipratham@gmail.com>
2024-11-19 18:48:11 +0530

    19/11/24

---
+++ LeetCode/Maximum Sum of Distinct Subarrays With Length K.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        l, r = 0, 0
+        maxSum, curr = 0, 0
+        s = set()
+
+        for r in range(n):
+            while nums[r] in s:
+                s.remove(nums[l])
+                curr -= nums[l]
+                l += 1
+
+            s.add(nums[r])
+            curr += nums[r]
+
+            if r - l + 1 == k:
+                maxSum = max(maxSum, curr)
+                s.remove(nums[l])
+                curr -= nums[l]
+                l += 1
+
+        return maxSum
