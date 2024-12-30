ee5c6f1fbe32bea66e1f3ee33126248f451f6aac
thepratholic <chelaramanipratham@gmail.com>
2024-12-30 17:00:48 +0530

    30/12/24

---
+++ LeetCode/Count Ways To Build Good Strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        mod = 10**9 + 7
+        dp = {}
+        def helper(length):
+            if length > high:
+                return 0
+            if length in dp:
+                return dp[length]
+
+            dp[length] = 1 if length >= low else 0
+            dp[length] += helper(length + zero) + helper(length + one)
+            return dp[length] % mod
+        return helper(0)
