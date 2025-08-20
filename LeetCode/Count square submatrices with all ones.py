cc0c3d791fe28e222c1a2661de9254a905dba87f
thepratholic <chelaramanipratham@gmail.com>
2025-08-20 21:07:40 +0530

    updated Code

--- LeetCode/Count square submatrices with all ones.py
+++ LeetCode/Count square submatrices with all ones.py
@@ -2,22 +2,38 @@ from typing import List
 
 
 class Solution:
+    def f(self, i, j, mat, m, n, dp):
+        if i >= m or j >= n:
+            return 0
+
+        if mat[i][j] == 0:
+            return 0
+
+        if dp[i][j] != -1:
+            return dp[i][j]
+
+        right = self.f(i, j + 1, mat, m, n, dp)
+        diag = self.f(i + 1, j + 1, mat, m, n, dp)
+        down = self.f(i + 1, j, mat, m, n, dp)
+
+        dp[i][j] = 1 + min(right, diag, down)
+        return dp[i][j]
+
     def countSquares(self, matrix: List[List[int]]) -> int:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        cache = {}
-        def dfs(r, c):
-            if r == ROWS or c == COLS or not matrix[r][c]:
-                return 0
-            if (r, c) in cache:
-                return cache[(r, c)]
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-            cache[(r, c)] = 1 + min(dfs(r+1, c), dfs(r, c+1), dfs(r+1, c+1))
 
-            return cache[(r, c)]
+        ans = 0
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if matrix[i][j] == 1:
+                    right = dp[i][j + 1]
+                    diag = dp[i + 1][j + 1]
+                    down = dp[i + 1][j]
 
-        res = 0
-        for i in range(ROWS):
-            for j in range(COLS):
-                res += dfs(i, j)
+                    dp[i][j] = 1 + min(right, diag, down)
+                    ans += dp[i][j]
 
-        return res
+        return ans
