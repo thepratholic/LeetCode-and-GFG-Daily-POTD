b8a17ef414486dd0495ae70211c403926a608c30
thepratholic <chelaramanipratham@gmail.com>
2025-09-29 16:25:37 +0530

    29/9/25

---
+++ LeetCode/Minimum Score Triangulation of Polygon.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        n = len(values)
+
+        # res = [inf]
+
+        def f(i, j):
+            if j - i + 1 < 3:
+                return 0
+
+            if dp[i][j] != -1:
+                return dp[i][j]
+
+            ans = inf
+            for k in range(i + 1, j):
+                t = values[i] * values[j] * values[k]
+                wt = f(i, k) + t + f(k, j)
+
+                ans = min(ans, wt)
+
+            dp[i][j] = ans
+            return dp[i][j]
+
+        dp = [[-1] * n for _ in range(n)]
+        return f(0, n - 1)
