43569929d65fcbc69d011c9bbfa28dd22356225b
thepratholic <chelaramanipratham@gmail.com>
2025-10-21 20:05:41 +0530

    21/10/25

---
+++ LeetCode/Maximum Frequency of an Element After Performing Operations I.py
@@ -0,0 +1,26 @@
+from bisect import bisect_left, bisect_right
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        n = len(nums)
+
+        maxi = float('-inf')
+
+        nums.sort()
+
+        freq = Counter(nums)
+        s = set()
+        for el in nums:
+            s.add(el)
+            s.add(el - k)
+            s.add(el + k)
+
+
+        for el in s:
+            count = bisect_right(nums, el + k) - bisect_left(nums, el - k)
+            maxi = max(maxi, freq[el] + min(count - freq[el], numOperations))
+
+        return maxi
