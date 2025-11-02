bd677e47e8d71b2474075057a7db64b9f055b3f2
thepratholic <chelaramanipratham@gmail.com>
2025-11-02 10:31:48 +0530

    Code Updated for Count Unguarded Cells in the Grid

--- LeetCode/Count unguarded cells in the grid.py
+++ LeetCode/Count unguarded cells in the grid.py
@@ -2,48 +2,25 @@ from typing import List
 
 
 class Solution:
-    UNGUARDED = 0
-    GUARDED = 1
-    GUARD = 2
-    WALL = 3
-
-    # Depth-First Search to mark guarded cells
-    def _dfs(self, row: int, col: int, grid: List[List[int]], direction: str) -> None:
-        if (
-            row < 0
-            or row >= len(grid)
-            or col < 0
-            or col >= len(grid[0])
-            or grid[row][col] == self.GUARD
-            or grid[row][col] == self.WALL
-        ):
-            return
-
-        grid[row][col] = self.GUARDED  # Mark cell as guarded
-        if direction == "U":
-            self._dfs(row - 1, col, grid, "U")  # Up
-        if direction == "D":
-            self._dfs(row + 1, col, grid, "D")  # Down
-        if direction == "L":
-            self._dfs(row, col - 1, grid, "L")  # Left
-        if direction == "R":
-            self._dfs(row, col + 1, grid, "R")  # Right
-
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        grid = [[self.UNGUARDED for _ in range(n)] for _ in range(m)]
+        unique_guards = {tuple(g) for g in guards}
+        unique_walls = {tuple(w) for w in walls}
 
-        for guard in guards:
-            grid[guard[0]][guard[1]] = self.GUARD
+        guarded = set()
 
-        for wall in walls:
-            grid[wall[0]][wall[1]] = self.WALL
+        dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-        # Mark cells as guarded by traversing from each guard
-        for guard in guards:
-            self._dfs(guard[0] - 1, guard[1], grid, "U")  # Up
-            self._dfs(guard[0] + 1, guard[1], grid, "D")  # Down
-            self._dfs(guard[0], guard[1] - 1, grid, "L")  # Left
-            self._dfs(guard[0], guard[1] + 1, grid, "R")  # Right
+        def isValid(i, j):
+            return i >= 0 and i < m and j >= 0 and j < n
 
-        count = sum(row.count(self.UNGUARDED) for row in grid)
-        return count
+        for r, c in unique_guards:
+            for x, y in dirs:
+                nRow, nCol = r + x, c + y
+
+                while isValid(nRow, nCol) and (nRow, nCol) not in unique_walls and (nRow, nCol) not in unique_guards:
+                    guarded.add((nRow, nCol))
+                    nRow += x
+                    nCol += y
+
+        empty = (m * n) - len(guards) - len(walls)
+        return empty - len(guarded)
