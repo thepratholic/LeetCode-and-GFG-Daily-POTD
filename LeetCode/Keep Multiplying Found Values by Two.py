9dd0abb281378a9a929a45e1971cb4b18bad7e1c
thepratholic <chelaramanipratham@gmail.com>
2025-11-19 06:59:40 +0530

    19/11/25

---
+++ LeetCode/Keep Multiplying Found Values by Two.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        
+        st = set(nums)
+
+        while original in st:
+            original *= 2
+
+        return original
