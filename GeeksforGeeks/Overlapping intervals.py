95080abf3e807e2c315352f11f6b526d0a397a8b
thepratholic <chelaramanipratham@gmail.com>
2025-12-17 20:45:37 +0530

    17/12/25

--- GeeksforGeeks/Overlapping intervals.py
+++ GeeksforGeeks/Overlapping intervals.py
@@ -1,22 +1,23 @@
 class Solution:
-    def mergeOverlap(self, arr):
-        arr.sort()
-
-        n = len(arr)
-
-        ans = []
-
-        i = 0
-
-        for interval in arr:
-            if len(ans) == 0:
-                ans.append(interval)
-            else:
-                if ans[i][1] > interval[1]:
-                    continue
-                elif ans[i][1] >= interval[0]:
-                    ans[i][1] = interval[1]
-                else:
-                    ans.append(interval)
-                    i += 1
-        return ans
+	def mergeOverlap(self, arr):
+		
+		ans = []
+		n = len(arr)
+		
+		arr.sort(key = lambda x : (x[0], x[1]))
+		
+		ans.append(arr[0])
+		
+		for i in range(1, n):
+		    if arr[i][0] <= ans[-1][-1]:
+		        
+		        if arr[i][1] <= ans[-1][-1]:
+		            continue
+		        
+		        elif arr[i][1] > ans[-1][-1]:
+		            ans[-1][-1] = arr[i][1]
+		            
+		    else:
+		        ans.append(arr[i])
+		        
+		return ans
