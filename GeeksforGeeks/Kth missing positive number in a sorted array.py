9d5013c035547b5a655f8cd1936b65419d10a615
thepratholic <chelaramanipratham@gmail.com>
2025-12-26 19:34:51 +0530

    26/12/25

--- GeeksforGeeks/Kth missing positive number in a sorted array.py
+++ GeeksforGeeks/Kth missing positive number in a sorted array.py
@@ -1,20 +1,16 @@
 class Solution:
     def kthMissing(self, arr, k):
-        # code here
-
-        n = len(arr)
-
-        low, high = 0, n - 1
-
-        while low <= high:
-            mid = (low + high) // 2
-
-            miss = arr[mid] - (mid + 1)
-
-            if miss < k:
-                low = mid + 1
-
+        
+        st = set(arr)
+        
+        num = 1
+        
+        while k > 0:
+            if num in st:
+                num += 1
+                
             else:
-                high = mid - 1
-
-        return low + k
+                num += 1
+                k -= 1
+                
+        return num - 1
