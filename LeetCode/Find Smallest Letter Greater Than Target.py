6aebed618a71c0ed4d85519ef366db94709ac0fe
thepratholic <chelaramanipratham@gmail.com>
2026-01-31 08:25:09 +0530

    31/1/26

---
+++ LeetCode/Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        n = len(letters)
+
+        for i in range(n):
+            if letters[i] > target:
+                return letters[i]
+
+        return letters[0]
